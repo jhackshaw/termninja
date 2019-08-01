0721a02f9dd86c77c2b1cb334bc8d1714c1ab311
Jeff Hackshaw <jeffrey.hackshaw@gmail.com>
2019-08-01 16:44:28 -0400

    symlink issue with letsencrypt certs

--- api/run.py
+++ api/run.py
@@ -6,12 +6,12 @@ from app import app
 if __name__ == "__main__":
     debug = os.environ.get('DEBUG', False)
     ssl = os.environ.get('TERMNINJA_API_SSL', None)
-    cert_path = os.environ.get('TERMNINJA_CERT_PATH', '/certs')
+    cert_path = os.environ.get('TERMNINJA_CERT_PATH', '/etc/letsencrypt')
 
     if ssl and os.path.exists(cert_path):
         ssl = {
-            'cert': f'{cert_path}/fullchain.pem',
-            'key': f'{cert_path}/privkey.pem'
+            'cert': f'{cert_path}/live/play.term.ninja/fullchain.pem',
+            'key': f'{cert_path}/live/play.term.ninja/privkey.pem'
         }
 
     app.run(host="0.0.0.0",
